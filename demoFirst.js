1fd35bcd8f9dd4e532b4d12832c5511712fe8669
kirannkasthuri-bit <kirannkasthuri@gmail.com>
2026-06-09 22:41:18 +0530

    Add files via upload
    First Assignment

---
+++ demoFirst.js
@@ -0,0 +1,27 @@
+let firstName = "kiran"
+console.log(firstName);
+console.log(typeof firstName);
+
+let companyName = "HCL"
+console.log(companyName);
+console.log(typeof companyName);
+
+let mobileNumber = "9442154660"
+console.log(mobileNumber);
+console.log(typeof mobileNumber);
+
+
+
+
+let isAutomation="true"
+console.log("success" + isAutomation);
+console.log(typeof isAutomation);
+
+let hasPlaywright
+console.log(hasPlaywright);
+console.log(typeof hasPlaywright);
+
+
+
+
+
